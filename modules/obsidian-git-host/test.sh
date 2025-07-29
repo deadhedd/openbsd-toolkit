d8366aaa23858d91a17b303b28d345b2c512f596
chris <cjw@startmail.com>
2025-07-28 19:46:57 -0700

    Updated test count

--- modules/obsidian-git-host/test.sh
+++ modules/obsidian-git-host/test.sh
@@ -122,12 +122,12 @@ check_entry() {
 ##############################################################################
 
 run_tests() {
-  echo "1..45"
+  echo "1..58"
 
-  ### Section 4) Packages
+  ### Section 4) Packages (1)
   run_test "command -v git"                                              "git is installed"
 
-  ### Section 5) Users & group
+  ### Section 5) Users & group (7)
   run_test "id ${OBS_USER}"                                              "user '${OBS_USER}' exists"
   assert_user_shell "${OBS_USER}" "/bin/ksh"                              "shell for '${OBS_USER}' is /bin/ksh"
   run_test "id ${GIT_USER}"                                              "user '${GIT_USER}' exists"
@@ -136,7 +136,7 @@ run_tests() {
   run_test "id -nG ${OBS_USER} | grep -qw vault"                         "user '${OBS_USER}' is in group 'vault'"
   run_test "id -nG ${GIT_USER} | grep -qw vault"                         "user '${GIT_USER}' is in group 'vault'"
 
-  ### Section 6) doas config
+  ### Section 6) doas config (6)
   run_test "[ -f /etc/doas.conf ]"                                          "doas.conf exists"
   run_test "grep -q \"^permit persist ${OBS_USER} as root\$\" /etc/doas.conf" \
            "doas.conf allows persist ${OBS_USER}"
@@ -149,12 +149,12 @@ run_tests() {
 
   ### Section 7) SSH hardening & per-user SSH dirs
 
-  # 7.1 SSH Service & Config
+  # 7.1 SSH Service & Config (2)
   run_test "grep -q \"^AllowUsers ${OBS_USER} ${GIT_USER}\$\" /etc/ssh/sshd_config" \
            "sshd_config has AllowUsers"
   run_test "pgrep -x sshd >/dev/null"                                       "sshd daemon running"
   
-  # 7.2 .ssh Directories and authorized users
+  # 7.2 .ssh Directories and authorized users (6)
   run_test "[ -d /home/${GIT_USER}/.ssh ]"                                  "ssh dir for ${GIT_USER} exists"
   assert_file_perm "/home/${GIT_USER}/.ssh" "700"                            "ssh dir perms for ${GIT_USER}"
   run_test "stat -f '%Su:%Sg' /home/${GIT_USER}/.ssh | grep -q '^${GIT_USER}:${GIT_USER}\$'" \
@@ -164,29 +164,29 @@ run_tests() {
   run_test "stat -f '%Su:%Sg' ${OBS_HOME}/.ssh | grep -q '^${OBS_USER}:${OBS_USER}\$'" \
            "ssh dir owner for ${OBS_USER}"
 
-  # authorized_keys for GIT_USER
+  # authorized_keys for GIT_USER (3)
   run_test "[ -f /home/${GIT_USER}/.ssh/authorized_keys ]"                           "authorized_keys for ${GIT_USER} exists"
   assert_file_perm "/home/${GIT_USER}/.ssh/authorized_keys" "600"                     "authorized_keys perms for ${GIT_USER}"
   run_test "stat -f '%Su:%Sg' /home/${GIT_USER}/.ssh/authorized_keys | grep -q '^${GIT_USER}:${GIT_USER}\$'" \
            "authorized_keys owner for ${GIT_USER}"
 
-  # authorized_keys for OBS_USER
+  # authorized_keys for OBS_USER (3)
   run_test "[ -f ${OBS_HOME}/.ssh/authorized_keys ]"                                  "authorized_keys for ${OBS_USER} exists"
   assert_file_perm "${OBS_HOME}/.ssh/authorized_keys"             "600"               "authorized_keys perms for ${OBS_USER}"
   run_test "stat -f '%Su:%Sg' ${OBS_HOME}/.ssh/authorized_keys | grep -q '^${OBS_USER}:${OBS_USER}\$'" \
            "authorized_keys owner for ${OBS_USER}"
 
-  # 7.3 Known Hosts (OBS_USER only)
+  # 7.3 Known Hosts (OBS_USER only) (4)
   run_test "[ -f ${OBS_HOME}/.ssh/known_hosts ]"                                      "known_hosts for ${OBS_USER} exists"
   assert_file_perm "${OBS_HOME}/.ssh/known_hosts"               "644"                  "known_hosts perms for ${OBS_USER}"
   run_test "stat -f '%Su:%Sg' ${OBS_HOME}/.ssh/known_hosts | grep -q '^${OBS_USER}:${OBS_USER}\$'" \
            "known_hosts owner for ${OBS_USER}"
   run_test "grep -q \"${GIT_SERVER}\" ${OBS_HOME}/.ssh/known_hosts"                   "known_hosts contains entry for ${GIT_SERVER}"
 
-  ### Section 8) Repo paths & bare init
+  ### Section 8) Repo paths & bare init (1)
   run_test "[ -d ${BARE_REPO} ]"                                             "bare repository exists"
 
-  ### Section 9) Git configs (safe.directory & sharedRepository)
+  ### Section 9) Git configs (safe.directory & sharedRepository) (5)
   check_entry "/home/${GIT_USER}/.gitconfig"       "${BARE_REPO}"             "${GIT_USER}"
   check_entry "/home/${GIT_USER}/.gitconfig"       "${OBS_HOME}/vaults/${VAULT}" "${GIT_USER}"
   check_entry "${OBS_HOME}/.gitconfig"             "${OBS_HOME}/vaults/${VAULT}" "${OBS_USER}"
@@ -194,7 +194,7 @@ run_tests() {
   run_test "grep -q '^[[:space:]]*sharedRepository = group\$' ${BARE_REPO}/config" \
            "config file sets 'sharedRepository = group' under [core]"
 
-  ### Section 10) Post-receive hook
+  ### Section 10) Post-receive hook (5)
   run_test "[ -x ${BARE_REPO}/hooks/post-receive ]"                          "post-receive hook executable"
   run_test "grep -q '^#!/bin/sh\$' ${BARE_REPO}/hooks/post-receive"           "hook shebang correct"
   run_test "grep -q \"^SHA=\\\$(cat \\\"${BARE_REPO}/refs/heads/master\\\")\\\$\" \"${BARE_REPO}/hooks/post-receive\"" \
@@ -203,14 +203,14 @@ run_tests() {
            "hook: git checkout command correct"
   run_test "grep -q '^exit 0\$' ${BARE_REPO}/hooks/post-receive"             "hook: exits cleanly"
 
-  ### Section 11) Working copy clone & initial commit
+  ### Section 11) Working copy clone & initial commit (3)
   run_test "[ -d ${OBS_HOME}/vaults/${VAULT}/.git ]"                          "working clone exists"
   run_test "su - ${OBS_USER} -c \"git -C ${OBS_HOME}/vaults/${VAULT} remote get-url origin | grep -q '${BARE_REPO}'\"" \
            "working clone origin correct"
   run_test "su - ${OBS_USER} -c \"git -C ${OBS_HOME}/vaults/${VAULT} log -1 --pretty=%B | grep -q 'initial commit'\"" \
            "initial commit present"
 
-  ### Section 12) Final perms on bare repo
+  ### Section 12) Final perms on bare repo (6)
   run_test "stat -f '%Su:%Sg' ${BARE_REPO} | grep -q '^${GIT_USER}:vault\$'"   "ownership of '${BARE_REPO}' is '${GIT_USER}:vault'"
   run_test "! find ${BARE_REPO} \( -not -user ${GIT_USER} -or -not -group vault \) -print | grep -q ." \
            "all files under '${BARE_REPO}' are owned by ${GIT_USER}:vault"
@@ -219,7 +219,7 @@ run_tests() {
   run_test "! find ${BARE_REPO} -type d -not -perm -g=x -print | grep -q ."  "all directories under '${BARE_REPO}' are group-executable"
   run_test "! find ${BARE_REPO} -type d -not -perm -g+s -print | grep -q ."  "all directories under '${BARE_REPO}' have the setgid bit set"
 
-  ### Section 13) History settings (.profile)
+  ### Section 13) History settings (.profile) (6)
   run_test "grep -q '^export HISTFILE=/home/${OBS_USER}/.ksh_history\$' /home/${OBS_USER}/.profile" \
            "HISTFILE export in ${OBS_USER} .profile"
   run_test "grep -q '^export HISTSIZE=5000\$' /home/${OBS_USER}/.profile"        "HISTSIZE export in ${OBS_USER} .profile"
